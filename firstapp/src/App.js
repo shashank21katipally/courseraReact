28f3673217715181bad8aaec99e79b77428d402c
shashank21katipally <shashank21katipally@gmail.com>
2024-02-25 13:09:52 -0500

    creating card component

--- firstapp/src/App.js
+++ firstapp/src/App.js
@@ -1,8 +1,18 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-import Heading from "./Heading";
+// import Heading from "./Heading";
+import Card from "./card";
 
-
+function App() {
+  return (
+    <div>
+      <h1>Task: Add three Card elements</h1>
+      <Card h2="First card's h2" h3="First card's h3"/>
+      <Card h2="Second card's h2" h3="Second card's h3"/>
+      <Card h2="Third card's h2" h3="Third card's h3"/>
+    </div>
+  );
+}
 
 // function Header() {
 //   return (
@@ -10,13 +20,13 @@ import Heading from "./Heading";
 //   )
 // }
 
-function App() {
-  return (
-    <div className="App">
-      <Heading firstName="Ballukam"/>
-      <Heading firstName="Bob"/>
-    </div>
-  );
-}
+// function App() {
+//   return (
+//     <div className="App">
+//       <Heading firstName="Ballukam"/>
+//       <Heading firstName="Bob"/>
+//     </div>
+//   );
+// }
 
 export default App;

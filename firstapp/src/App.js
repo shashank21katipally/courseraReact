92fb947666aa089235afd1cfb1c84676e2d3427f
shashank21katipally <shashank21katipally@gmail.com>
2024-02-24 19:20:04 -0500

    creating first functional component and render it through root app

---
+++ firstapp/src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+
+// function Heading() {
+//   let title = 'This is some heading text';
+//   return (
+//     <h1>{title}</h1>
+//   );
+// }
+
+function Header() {
+  return (
+    <h1>Hello World!</h1>
+  )
+}
+
+function App() {
+  return (
+    // <Heading/>
+    <Header />
+  );
+}
+
+export default App;
